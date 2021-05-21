4ed717c77e5e5322a924496453162fdadce2331d
Squid233 <60126026+squid233@users.noreply.github.com>
2021-05-21 19:49:02 +0800

    Create Blocks.java

---
+++ src/main/java/io/github/chemiscraft/block/Blocks.java
@@ -0,0 +1,10 @@
+package io.github.chemiscraft.block;
+
+import net.minecraft.block.Block;
+
+public class Blocks {
+    public static final Block = new Block(settings());
+
+    /** Load class */
+    public static void register() { }
+}
