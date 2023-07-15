829b1fd0388266f1cf247a0b5a9ce12ae467e7c0
crazy_piggy_ <57442208+crazy-piggy@users.noreply.github.com>
2023-07-15 14:00:44 +0800

    Update

--- src/main/java/io/github/biochemiscraft/block/Blocks.java
+++ src/main/java/io/github/biochemiscraft/block/Blocks.java
@@ -3,7 +3,6 @@ package io.github.biochemiscraft.block;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.FluidBlock;
-import net.minecraft.block.Material;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
@@ -12,14 +11,14 @@ import static io.github.biochemiscraft.fluid.Fluids.PHOSPHORICACID_STILL;
 import static net.minecraft.block.Blocks.WATER;
 
 public class Blocks {
-    public static final IronTrivet IRON_TRIVET = new IronTrivet(FabricBlockSettings.of(Material.METAL).hardness(0.1f));
-    public static final IronStand IRON_STAND = new IronStand(FabricBlockSettings.of(Material.METAL).hardness(0.1f));
-    public static final Block PHOSPHORUS_ORE = new Block(FabricBlockSettings.of(Material.METAL).hardness(1.0f));
-    public static final PhosphorusBlock WHITE_PHOSPHORUS_BLOCK = new PhosphorusBlock(FabricBlockSettings.of(Material.METAL).hardness(1.5f));
-    public static final PhosphorusBlock RED_PHOSPHORUS_BLOCK = new PhosphorusBlock(FabricBlockSettings.of(Material.METAL).hardness(1.5f));
-    public static final SJOL SOUL_JACK_O_LANTERN = new SJOL(FabricBlockSettings.of(Material.METAL).hardness(1.5f));
+    public static final IronTrivet IRON_TRIVET = new IronTrivet(FabricBlockSettings.create().hardness(0.1f));
+    public static final IronStand IRON_STAND = new IronStand(FabricBlockSettings.create().hardness(0.1f));
+    public static final Block PHOSPHORUS_ORE = new Block(FabricBlockSettings.create().hardness(1.0f));
+    public static final PhosphorusBlock WHITE_PHOSPHORUS_BLOCK = new PhosphorusBlock(FabricBlockSettings.create().hardness(1.5f));
+    public static final PhosphorusBlock RED_PHOSPHORUS_BLOCK = new PhosphorusBlock(FabricBlockSettings.create().hardness(1.5f));
+    public static final SJOL SOUL_JACK_O_LANTERN = new SJOL(FabricBlockSettings.create().hardness(1.5f));
     public static final Block PHOSPHORICACID = new FluidBlock(PHOSPHORICACID_STILL, FabricBlockSettings.copy(WATER));
-    public static final GlassMeltingFurnace GLASS_MELTING_FURNACE = new GlassMeltingFurnace(FabricBlockSettings.of(Material.METAL).hardness(3.75f));
+    public static final GlassMeltingFurnace GLASS_MELTING_FURNACE = new GlassMeltingFurnace(FabricBlockSettings.create().hardness(3.75f));
 
     public static void register() {
         register("iron_stand", IRON_STAND);
